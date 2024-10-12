83ce33159c05031f99b40fb1bf64a6a5d0771579
IgorOK96 <IgorOK96@users.noreply.github.com>
2024-10-12 14:10:05 +0200

    Commit all-in 1.1

--- TrafficLight UI/ContentView.swift
+++ TrafficLight UI/ContentView.swift
@@ -7,18 +7,86 @@
 
 import SwiftUI
 
+enum CurrentLight {
+    case red
+    case yellow
+    case green
+}
+
 struct ContentView: View {
+    @State private var currentLight: CurrentLight? = nil
+    @State private var buttonText = "START"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.cyan
+                .ignoresSafeArea()
+            ZStack {
+                VStack() {
+                    Color.black
+                        .frame(width: 220, height: 520)
+                        .cornerRadius(20)
+                        .offset(y: -50)
+                    
+                    Button(action: traffic) {
+                        Text(buttonText)
+                            .font(.title)
+                            .foregroundStyle(.cyan)
+                            .frame(width: 200, height: 50)
+                            .background(Color.white)
+                            .cornerRadius(20)
+                            .offset(y: 35)
+                    }
+                }
+                VStack {
+                    CircleView(color: .red, isActive: currentLight == .red)
+                    CircleView(color: .yellow, isActive: currentLight == .yellow)
+                    CircleView(color: .green, isActive: currentLight == .green)
+                }
+                .offset(y: -85)
+            }
         }
-        .padding()
+    }
+    
+    struct CircleView: View {
+        var color: Color
+        var isActive: Bool
+        
+        var body: some View {
+            ZStack {
+                Circle()
+                    .foregroundStyle(.gray)
+                    .frame(width: 140)
+                Circle()
+                    .foregroundStyle(color)
+                    .frame(width: 115)
+                    .opacity(isActive ? 1.0 : 0.4)
+            }
+        }
+    }
+    
+    private func traffic() {
+        if currentLight == nil {
+            buttonText = "NEXT"
+            currentLight = .red
+        } else {
+            switch currentLight {
+            case .red:
+                currentLight = .yellow
+            case .yellow:
+                currentLight = .green
+            case .green:
+                currentLight = .red
+            default:
+                break
+            }
+        }
     }
 }
 
 #Preview {
     ContentView()
 }
+
+
+
