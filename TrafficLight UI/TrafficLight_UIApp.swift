468616fecca9a5f4de5a370917a077c02c534b53
IgorOK96 <IgorOK96@users.noreply.github.com>
2024-10-12 11:49:36 +0200

    Initial Commit

---
+++ TrafficLight UI/TrafficLight_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLight_UIApp.swift
+//  TrafficLight UI
+//
+//  Created by user246073 on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLight_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
